bcf8d5bcf9280f888ff1a4af54b98c29ff148764
misti <sa@>
2025-11-24 20:13:03 +0500

    Создал библиотекy классов. Добавил клаасс Leaders и Snakes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Snake
 {
-    internal class Program
+    public class Program
     {
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        public static List<Leaders> LeadersList = new List<Leaders>();
+        public static int localPort = 5001;
+        public static int MaxSpeed = 15;
         static void Main(string[] args)
         {
         }
